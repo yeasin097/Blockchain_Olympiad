56a656c596784235db4428f277cbf5826516fdec
ishraqs19 <88086562+ishraqs19@users.noreply.github.com>
2024-08-28 02:01:05 +0600

    Create ProductForInspectionI.js

---
+++ src/Pages/Inspector/ProductForInspectionI.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+import Product_Inspection_I from '../../Components/Inspector/Product_Inspection_I';
+
+const ProductForInspectionI = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Product_Inspection_I/>
+        </div>
+    );
+};
+
+export default ProductForInspectionI;

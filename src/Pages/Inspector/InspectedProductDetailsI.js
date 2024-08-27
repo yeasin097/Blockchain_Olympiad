eccc9066b589975abe51f1e1f3b2b875183f29c4
ishraqs19 <88086562+ishraqs19@users.noreply.github.com>
2024-08-28 01:59:53 +0600

    Create InspectedProductDetailsI.js

---
+++ src/Pages/Inspector/InspectedProductDetailsI.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+import InspectedProduct_Details from '../../Components/Inspector/InspectedProduct_Details';
+
+const InspectedProductDetails = () => {
+    return (
+        <div>
+            <Navbar/>
+            <InspectedProduct_Details/>
+        </div>
+    );
+};
+
+export default InspectedProductDetails;

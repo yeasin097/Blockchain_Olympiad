898da5d1885165628086e72b01f068797e2ae6e7
ishraqs19 <88086562+ishraqs19@users.noreply.github.com>
2024-08-28 02:00:22 +0600

    Create Inspectors.js

---
+++ src/Pages/Inspector/Inspectors.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+import LogoSearchBar from '../../Components/LogoSearchBar/LogoSearchBar';
+import InspectorOption_I from '../../Components/Inspector/InspectorOption_I';
+const Inspectors = () => {
+    return (
+        <div>
+            <Navbar />
+            <LogoSearchBar />
+            <InspectorOption_I />
+            
+        </div>
+    );
+};
+
+export default Inspectors;

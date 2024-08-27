ec319b39b29d977d1fef8579ba8e12f2c0d2cde8
ishraqs19 <88086562+ishraqs19@users.noreply.github.com>
2024-08-28 01:39:43 +0600

    Create Accountdetails_I.js

---
+++ src/Components/Inspector/Accountdetails_I.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Accountdetails_I.css';
+
+const Accountdetails_I = () => {
+    return (
+        <div className="account-page">
+            <div className="account-details-container">
+                <form className="account-form">
+                    <div className="form-group">
+                        <label htmlFor="mobileNumber">Mobile Number</label>
+                        <input type="text" id="mobileNumber" className="form-control" placeholder="1537703400" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="firstName">First name *</label>
+                        <input type="text" id="firstName" className="form-control" placeholder="Urmi" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="lastName">Last name *</label>
+                        <input type="text" id="lastName" className="form-control" placeholder="Biswas" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="displayName">Display name *</label>
+                        <input type="text" id="displayName" className="form-control" placeholder="854245583157" />
+                        <small className="form-text text-muted">
+                            This will be how your name will be displayed in the account section and in reviews
+                        </small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email address *</label>
+                        <input type="email" id="email" className="form-control" placeholder="Enter your email" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="currentPassword">Current password (leave blank to leave unchanged)</label>
+                        <input type="password" id="currentPassword" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="newPassword">New password (leave blank to leave unchanged)</label>
+                        <input type="password" id="newPassword" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="confirmPassword">Confirm new password</label>
+                        <input type="password" id="confirmPassword" className="form-control" />
+                    </div>
+                    <Link to="/inspector">
+                        <button type="submit" className="button-accountdetails">Save changes</button>
+                    </Link>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Accountdetails_I;

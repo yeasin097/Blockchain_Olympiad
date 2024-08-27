0f69ebe2361340eb682692c69e04b46d23f79078
ishraqs19 <88086562+ishraqs19@users.noreply.github.com>
2024-08-28 01:28:30 +0600

    Create index.js

---
+++ src/Components/Admin/API/index.js
@@ -0,0 +1,13 @@
+
+
+export const getRevenue = () => {
+  return fetch("https://dummyjson.com/carts").then((res) => res.json());
+};
+
+
+export const getCustomers = () => {
+  return fetch("https://dummyjson.com/users").then((res) => res.json());
+};
+export const getComments = () => {
+  return fetch("https://dummyjson.com/comments").then((res) => res.json());
+};

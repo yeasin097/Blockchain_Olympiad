7b00ae695e73182fc5756f82eea84f8fe4e87cbf
ishraqs19 <88086562+ishraqs19@users.noreply.github.com>
2024-08-28 01:31:21 +0600

    Create index.js

---
+++ src/Components/Admin/PageContent/index.js
@@ -0,0 +1,10 @@
+import AppRoutes from "D:/Blockchain/Blockchain_Olympiad/src/Components/Admin/AppRoutes";
+
+function PageContent() {
+  return (
+    <div className="PageContent">
+      <AppRoutes />
+    </div>
+  );
+}
+export default PageContent;
